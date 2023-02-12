ea20054aee92dbba70fdb217248803aea53919bc
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2023-02-13 00:12:23 +0300

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,20 @@
 
 namespace NjoguAmos\KeMobile\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use NjoguAmos\KeMobile\KeMobileServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'NjoguAmos\\KenyaMobileRule\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             KeMobileServiceProvider::class,
         ];
     }
 
-    public function getEnvironmentSetUp($app)
+    public function getEnvironmentSetUp($app): void
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-kenya-mobile_table.php.stub';
-        $migration->up();
-        */
     }
 }
