c37f1aadf5109334a0fe424c3dc32c4008642ff0
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-11-23 21:34:21 +0300

    fix: php stan error

--- src/KenyaMobileRule.php
+++ src/KenyaMobileRule.php
@@ -2,11 +2,12 @@
 
 namespace NjoguAmos\KeMobile;
 
-use Illuminate\Contracts\Validation\InvokableRule;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
 
-class KenyaMobileRule implements InvokableRule
+class KenyaMobileRule implements ValidationRule
 {
-    public function __invoke($attribute, $value, $fail)
+    public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         if (! preg_match(pattern: '/^0?[17][0-9]{8}$/', subject: $value)) {
             $fail(trans(key: 'kenya-mobile::kenya-mobile.phone'));
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Contracts\Validation\InvokableRule;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
 use NjoguAmos\KeMobile\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
@@ -9,17 +10,19 @@ expect()
     ->extend(name: 'toPassWith', extend: function (mixed $value) {
         $rule = $this->value;
 
-        if (! $rule instanceof InvokableRule) {
+        if (! $rule instanceof ValidationRule) {
             throw new Exception(message: 'Value is not an invokable rule');
         }
 
         $passed = true;
 
-        $fail = function () use (&$passed) {
+        $fail = function (?string $message = null) use (&$passed) {
             $passed = false;
+
+            return new PotentiallyTranslatedString($message ?? 'attribute', app('translator'));
         };
 
-        $rule(attribute: 'attribute', value: $value, fail: $fail);
+        $rule->validate('attribute', $value, $fail);
 
         expect(value: $passed)->toBeTrue();
     });
