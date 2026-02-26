a894d1bba99bd8e84a023d61b5facc1809ec0a09
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-02-26 18:04:27 +0300

    Guard from preg_match error on non string

--- src/KenyaMobileFullRule.php
+++ src/KenyaMobileFullRule.php
@@ -9,7 +9,7 @@ class KenyaMobileFullRule implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (! preg_match(pattern: '/^\+?254[17][0-9]{8}$/', subject: $value)) {
+        if (! is_string($value) || ! preg_match(pattern: '/^\+?254[17][0-9]{8}$/', subject: $value)) {
             $fail(trans(key: 'kenya-mobile::kenya-mobile.phone_full'));
         }
     }
